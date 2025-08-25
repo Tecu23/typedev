a6420d17cb9c3511031cdb5eace2d631b8950d53
tecu23 <andrei.teculescu@gmail.com>
2025-08-25 22:16:05 +0300

    addwords container wip

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -1,10 +1,9 @@
 import Caret from "./Caret";
+import WordsContainer from "./WordsContainer";
 
 type Props = {};
 
 const TypingTest = (props: Props) => {
-  const words =
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit proin convallis ex id vestibulum auctor Vivamus rutrum ultrices feugiat Vivamus augue est interdum sed nisi molestie gravida pharetra nulla Sed quis tellus quam Curabitur scelerisque ac elit ac auctor Nullam lacinia lobortis ipsum a posuere Donec tempor turpis nunc id eleifend purus suscipit in Curabitur euismod id risus eu rutrum Phasellus eu est turpis Nulla sit amet tempor diam Nunc porta ex nisi Maecenas vitae velit nec nulla pellentesque auctor ac id lacus Curabitur eros urna mollis blandit lectus eget aliquet commodo lorem Maecenas congue velit vitae rutrum sollicitudin. In hac.";
   return (
     <div
       id="typing_test"
@@ -21,24 +20,7 @@ const TypingTest = (props: Props) => {
         />
         <div id="out-of_focus"></div>
         <Caret />
-        <div
-          id="words"
-          className="col-[full-width] h-fit pb-2 flex flex-wrap w-full content-start select-none"
-        >
-          {words.split(" ").map((word, wordIndex) => {
-            return (
-              <div className="relative text-[32px] leading-[32px] mx-[0.3em] my-[0.25em] border-b-2 border-transparent">
-                {word.split("").map((letter, letterIndex) => {
-                  return (
-                    <div className="text-sub inline-block border-b-[0.05em] border-b-transparent">
-                      {letter}
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </div>
+        <WordsContainer />
       </div>
       <div id="restart_button"></div>
     </div>
---
+++ src/utils/constants/typing.ts
@@ -0,0 +1,2 @@
+export const CHARS_PER_LINE = 78;
+export const LINE_HEIGHT = 32;
