98177e7108c5bd236c7077c7a14695940ea3eaec
tecu23 <andrei.teculescu@gmail.com>
2025-08-26 20:18:24 +0300

    update footer design"

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,80 +1,93 @@
-import { Camera } from "lucide-react";
+import {
+  Camera,
+  CodeIcon,
+  FileIcon,
+  GitBranchIcon,
+  GithubIcon,
+  HandHelpingIcon,
+  HeadsetIcon,
+  LockIcon,
+  MailIcon,
+  PaletteIcon,
+  ShieldBanIcon,
+  TwitterIcon,
+} from "lucide-react";
 
 type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className="col-[content] relative text-center leading-4 text-xs">
+    <footer className="col-[content] relative text-center leading-4 text-xs text-sub">
       <div className="mb-8 leading-[2]">
-        <div className="bg-red-200 text-blue-500 px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3">
+        <div className="bg-sub text-bg px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3 text-xs">
           tab
         </div>
         {" + "}
-        <div className="bg-red-200 text-blue-500 px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3">
+        <div className="bg-sub text-bg px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3 text-xs">
           enter
         </div>
         {" - restart test "}
         <br />
-        <div className="bg-red-200 text-blue-500 px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3">
+        <div className="bg-sub text-bg px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3 text-xs">
           esc
         </div>
         {" or "}
-        <div className="bg-red-200 text-blue-500 px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3">
+        <div className="bg-sub text-bg px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3 text-xs">
           ctrl
         </div>
         {" + "}
-        <div className="bg-red-200 text-blue-500 px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3">
+        <div className="bg-sub text-bg px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3 text-xs">
           shift
         </div>
         {" + "}
-        <div className="bg-red-200 text-blue-500 px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3">
+        <div className="bg-sub text-bg px-[0.3rem] py-[0.1rem] rounded-[0.1rem] inline-block mx-[0.5rem] leading-3 text-xs">
           p
         </div>
         {" - command line "}
       </div>
-      <div className="grid grid-cols-2 gap-8">
-        <div className="flex flex-row flex-wrap">
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
+      <div className="grid grid-cols-[1fr_max-content] gap-8">
+        <div className="grid grid-cols-[repeat(4,auto)] lg:flex lg:flex-row justify-start w-max">
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <MailIcon size={15} />
             <p>contact</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <HandHelpingIcon size={15} />
+            <p>support</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <CodeIcon size={15} />
+            <p>github</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <HeadsetIcon size={15} />
+            <p>discord</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <TwitterIcon size={15} />
+            <p>twitter</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <FileIcon size={15} />
+            <p>terms</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <ShieldBanIcon size={15} />
+            <p>security</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <LockIcon size={15} />
+            <p>privacy</p>
           </button>
         </div>
-        <div className="flex flex-col xl:flex-wrap items-end">
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+        <div className="grid grid-cols-1 lg:flex justify-self-end text-right w-max h-max">
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <PaletteIcon size={15} />
+            <p>catpuccin</p>
           </button>
-          <button className="flex flex-row items-center gap-1 px-2 py-1">
-            <Camera />
-            <p>contact</p>
+          <button className="flex gap-1 py-[3px] px-1.5 justify-start items-center h-min w-max text-xs leading-3">
+            <GitBranchIcon size={15} />
+            <p>v1.0.0</p>
           </button>
         </div>
       </div>
